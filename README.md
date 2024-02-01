625bbd5e1fde3f2a5773bc615ef73994c661773a
munvoseli <@.>
2024-02-01 13:35:06 -0500

    unarchive

---
+++ README.md
@@ -0,0 +1,39 @@
+# I want to see my archive's files.
+
+usage: `iwts.py a.zip.tar.gz.xz.wahtever` and then u get a folder in your working directory
+
+have you ever unzipped a zip file and the contents splattered all over your directory like ketchup?
+
+have you ever extracted a tar archive and the permissions were so messed up
+you had to use `sudo rm -rf` or `chmod -R` to remove it even though you made sure to use
+`--no-same-owner` and `--no-same-permissions`?
+
+this is the program for you!!!!!
+
+name inspired by https://moonbase.lgbt/blog/i-want-to-see-my-friends-posts/ which isn't really relevent
+in the context this was made
+but im just tired of things like (insert library) being absolutely esoteric
+and (insert wrapper for library) having documentation which is literally full of lies
+and is otherwise unhelpful
+and (insert command line tool) having enough options that it takes more than 20 minutes to go through
+in a domain with so much potential vocabulary that you need a thesaurus if you're gonna search for a feature.
+
+This program is entirely a convenience wrapper over archive extraction.
+
+Simple.  Pona.
+
+* if the entries in the archive aren't all in one folder, it will put them all in one folder.
+* regarding permissions and ownership in tar archives,
+    * directory permissions will be set to 755
+    * file read permissions will be enabled
+    * file user-write permissions will be enabled
+    * file execute permissions will be preserved
+    * owner will not be preserved
+
+usage: `iwts.py a.zip.tar.gz.xz.wahtever`
+
+depends on `unzip` and `tar` and hopefully more things eventually
+
+there are no command line options at this time.  future options may include:
+* destination directory
+* maybe listing contents of archive?
---
+++ iwts.py
@@ -0,0 +1,99 @@
+#!/bin/python3
+
+
+import subprocess
+import sys
+from os import path as ospath
+from os import listdir
+import os
+import tempfile
+import shutil
+import stat
+
+def cmd_to_stdout_str(cmd):
+	return subprocess.run(cmd, stdout=subprocess.PIPE).stdout.decode('utf-8')
+
+#def get_folder(line):
+#	i = line.find('/')
+#	if i == -1:
+#		return line
+#	else:
+#		return line[0:i]
+#def zip_are_all_files_in_same_folder(fname):
+#	files = cmd_to_stdout_str(['zip', '-sf', fname]).splitlines()
+#	files.pop() # get rid of the "Total n files" line
+#	files.pop(0) # get rid of the "Archive contains" line
+#	if len(files) == 0:
+#		return True
+#	prefix = get_folder(files[0])
+#	for f in files:
+#		if get_folder(f) != prefix:
+#			return False
+#	return True
+
+def mk_dir_get_path(fname):
+	base = ospath.splitext(ospath.split(fname)[1])[0]
+	foldername = base
+	i = 2
+	while ospath.exists(foldername):
+		foldername = f'{base}-{i}'
+		i += 1
+	path = f'./{foldername}'
+	subprocess.run(['mkdir', path])
+	return path
+
+def extract_zip(fname):
+	path = mk_dir_get_path(fname)
+	subprocess.run(['unzip', fname, '-d', path])
+	maybe_flatten(path)
+
+def extract_tar(fname):
+	path = mk_dir_get_path(fname)
+	subprocess.run(['tar', 'xf', fname, '-C', path, '--no-same-owner'])
+	# mess with permissions, because --no-same-permissions is bad
+	for p, dirs, files in os.walk(path):
+		os.chmod(p, 0o755)
+		for f in files:
+			fpath = ospath.join(p, f)
+			m = os.stat(fpath).st_mode
+			xperms = m & (stat.S_IXUSR | stat.S_IXGRP | stat.S_IXOTH)
+			os.chmod(fpath, 0o644 | xperms)
+	maybe_flatten(path)
+
+# if the provided directory contains only one entry,
+# and that entry is a directory,
+# move everything out of the subdirectory and into the directory
+def maybe_flatten(dir_path):
+	l = listdir(dir_path)
+	if len(l) != 1:
+		return False
+	subentry_path = ospath.join(dir_path, l[0])
+	if not ospath.isdir(subentry_path):
+		return False
+	subdir_path = subentry_path
+	# make tmp dir
+	tmpdir_path = tempfile.mkdtemp()
+	subtmpdir_path = ospath.join(tmpdir_path, "a")
+	shutil.move(subdir_path, subtmpdir_path)
+	# move the files
+	entries = listdir(subtmpdir_path)
+	for e in entries:
+		oldepath = ospath.join(subtmpdir_path, e)
+		newepath = ospath.join(dir_path, e)
+		shutil.move(oldepath, newepath)
+	# clean up tmp dir
+	shutil.rmtree(tmpdir_path)
+	return True
+
+
+def extract(fname):
+	# TODO: better file detection
+	if fname.endswith('.zip') or fname.endswith('.epub') or fname.endswith('.htmlz'):
+		extract_zip(fname)
+	if fname.endswith('.tar.gz') or fname.endswith('.tar.xz'):
+		extract_tar(fname)
+	else:
+		print(f'file extension unknown')
+
+if __name__ == "__main__":
+	extract(sys.argv[1])
